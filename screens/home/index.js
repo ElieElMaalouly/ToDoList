399280124a8d4cda57cae71ceaa7aa02aad083e3
ElieElMaalouly <elie.maalouly@postlight.com>
2018-01-02 15:14:52 +0200

    feat: create todo list with add task

--- screens/home/index.js
+++ screens/home/index.js
@@ -12,8 +12,46 @@ import {
 import firebase from "react-native-firebase";
 
 export default class HomeScreen extends React.Component {
+  static navigatorButtons = {
+    rightButtons: [
+      {
+        title: "Add",
+        id: "btnAdd"
+      }
+    ]
+  };
+
   constructor(props) {
     super(props);
+    this.state = {
+      todoTasks: [],
+      isLoading: false
+    };
+    this.ref = firebase.firestore().collection("todoTasks");
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+  }
+
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(querySnapshot => {
+      const todos = [];
+      querySnapshot.forEach(doc => {
+        todos.push({
+          taskName: doc.data().taskName
+        });
+      });
+      this.setState({ todoTasks: todos, isLoading: false });
+    });
+  }
+
+  onNavigatorEvent(event) {
+    if (event.type == "NavBarButtonPress") {
+      if (event.id == "btnAdd") {
+        this.props.navigator.push({
+          screen: "todolist.addtaskscreen",
+          title: "Add Task"
+        });
+      }
+    }
   }
 
   render() {
@@ -25,7 +63,19 @@ export default class HomeScreen extends React.Component {
           backgroundColor: "white",
           borderRadius: Platform.OS === "ios" ? 30 : 0
         }}
-      />
+      >
+        <FlatList
+          data={this.state.todoTasks}
+          renderItem={({ item, index }) => {
+            return (
+              <Text style={{ fontSize: 20, fontWeight: "bold", margin: 20 }}>
+                {item.taskName}
+              </Text>
+            );
+          }}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
     );
   }
 }
--- screens/index.js
+++ screens/index.js
@@ -4,10 +4,10 @@ import { Navigation } from "react-native-navigation";
 
 import LoginScreen from "./login";
 import HomeScreen from "./home";
-import AddToDoScreen from "./addtodo";
+import AddTaskScreen from "./addtask";
 
 export function registerScreens() {
   Navigation.registerComponent("todolist.loginscreen", () => LoginScreen);
   Navigation.registerComponent("todolist.homescreen", () => HomeScreen);
-  Navigation.registerComponent("todolist.addtodoscreen", () => AddToDoScreen);
+  Navigation.registerComponent("todolist.addtaskscreen", () => AddTaskScreen);
 }
